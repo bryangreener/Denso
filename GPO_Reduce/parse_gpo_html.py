cfd9d11b61b1433ec0f1901f3d4ff129f9825c29
Bryan Greener <bryangreener@users.noreply.github.com>
2018-07-19 16:30:08 -0400

    Update parse_gpo_html.py
    Update to fix html output. Program now deletes unnecessary fields from html, color coordinates, and reworks general field to inform user of color key. Still need to handle sub-table parsing. Need to make code more efficient as it takes forever to run. Need to comment further and clean up code.

--- GPO_Reduce/parse_gpo_html.py
+++ GPO_Reduce/parse_gpo_html.py
@@ -5,13 +5,11 @@
 
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
-from xml.etree import ElementTree as ET
 from pathlib import Path
 
 import argparse
 
 #### TODO
-#    Update HTML generator to output prettier page.
 #    Fix parsing of nested tables.
 #    Clean up code and comment more.
 
@@ -73,15 +71,9 @@ def build_tree_util(root, leaf_list):
             child.inner_HTML = str(content).replace('\n', '')
             # List of all leaf nodes to make comparison easier
             leaf_list.append(child)
-            # Populate path to root for leaf node (makes life easier later)
-            temp = child
-            temp_list = []
-            while temp.parent:
-                temp_list.append(temp.name)
-                temp = temp.parent
-            child.path = temp_list
+            
             #### FIXME
-            #### Need to account for subtables recursively.
+            #### Need to account for sub-tables recursively.
             # Populate table of data at leaf node
             tables = content.find_all('table')
             child.table.append(tables)
@@ -99,6 +91,15 @@ def build_tree_util(root, leaf_list):
                 child.children[-1].parent = child
                 child.children[-1].data = i.find_previous_sibling('div').find_next('span', class_='sectionTitle')
                 child.children[-1].name = child.children[-1].data.string
+                
+        # Populate path to root for node (makes life easier later)
+        temp = child
+        temp_list = []
+        while temp.parent:
+            temp_list.append(temp.name)
+            temp = temp.parent
+        child.path = temp_list
+            
         build_tree_util(child, leaf_list)
 
 # Element-wise comparison between both trees going both directions.
@@ -118,25 +119,25 @@ def compare_trees(leaf_list1, leaf_list2):
                         if row_j[0] in [x[0] for x in ii.table[1:]]:
                             if row_j == ii.table[1:][[x[0] for x in ii.table[1:]].index(row_j[0])]:
                                 results.append([row_j[0], '='])
-                                update_html(ii.table[0], row_j[0], '=')
+                                update_html(ii.table[0], row_j, '=')
                             else:
                                 results.append([row_j[0], '!'])
-                                update_html(ii.table[0], row_j[0], '!')
+                                update_html(ii.table[0], row_j, '!')
                         else:
                             results.append([row_j[0], 1])
-                            update_html(jj.table[0], row_j[0], '1')
+                            update_html(ii.table[0], row_j, '1')
                 for row_j in jj.table[1:]:
                     for row_i in ii.table[1:]:
                         if row_i[0] in  [x[0] for x in jj.table[1:]]:
                             if row_i == jj.table[1:][[x[0] for x in jj.table[1:]].index(row_i[0])]:
                                 results.append([row_i[0], '='])
-                                update_html(jj.table[0], row_i[0], '=')
+                                update_html(ii.table[0], row_i, '=')
                             else:
                                 results.append([row_i[0], '!'])
-                                update_html(jj.table[0], row_i[0], '!')
+                                update_html(ii.table[0], row_i, '!')
                         else:
                             results.append([row_i[0], 2])
-                            update_html(ii.table[0], row_i[0], 2)
+                            update_html(ii.table[0], row_i, 2)
                 # Remove duplicates created by process above
                 temp = []
                 for r in results:
@@ -147,6 +148,135 @@ def compare_trees(leaf_list1, leaf_list2):
                     ii.comparisons = comparisons[-1]
     return comparisons
 
+def update_html(tables, row, comparison):
+    for table in tables:
+        for r in table.find_all('tr'):
+            if row[0] in [x.string for x in r.find_all('th')]:
+                return # Skip headers
+            if row[0] in [x.string for x in r.find_all('td')]:
+                if str(comparison) == '1':
+                    r['style'] = 'background:#BB8FCE'
+                elif str(comparison) == '2':
+                    r['style'] = 'background:#F1948A'
+                elif str(comparison) == '!':
+                    r['style'] = 'background:#F7DC6F'
+                elif str(comparison) == '=':
+                    r['style'] = 'background:#82E0AA'
+                return
+    # If row doesnt exist in html 1, add row.
+    soup = BeautifulSoup('', 'lxml')
+    new_tr = soup.new_tag('tr')
+    for i in row:
+            new_td = soup.new_tag('td')
+            if i:
+                new_td.string = i
+            else:
+                new_td.string = ''
+            new_tr.append(new_td)
+            if str(comparison) == '1':
+                new_tr['style'] = 'background:#BB8FCE'
+            elif str(comparison) == '2':
+                new_tr['style'] = 'background:#F1948A'
+    tables[0].append(new_tr)
+
+def update_html_general_section(soup, gpo1, gpo2):
+    s = soup.find('div', class_='gposummary')
+    s = s.next_element.find_next_sibling('div', class_='container')
+    # Delete useless fields, leaving first table in details section.
+    for i in [x for x in s.next_element.find_next_sibling('div', class_='container').next_siblings]:
+        if i == '\n':
+            i = ''
+        else:
+            i.decompose()
+    
+    t = s.find('table')
+    s = t.parent
+    # Delete old table.
+    t.decompose()
+    
+    soup = BeautifulSoup('', 'lxml')
+    class_attr = {'class': 'info3', 'style': 'width:auto'}
+    table = soup.new_tag('table', **class_attr)
+    # Headers for GPO titles
+    tr = soup.new_tag('tr')
+    td = soup.new_tag('th', scope='col')
+    td.string = "GPO 1"
+    tr.append(td)
+    td = soup.new_tag('th', scope='col')
+    td.string = "GPO 2"
+    tr.append(td)
+    table.append(tr)
+    # Row for GPO titles
+    tr = soup.new_tag('tr')
+    td = soup.new_tag('td')
+    td.string = gpo1
+    tr.append(td)
+    td = soup.new_tag('td')
+    td.string = gpo2
+    tr.append(td)
+    table.append(tr)
+    
+    s.append(table)
+    
+    table = soup.new_tag('table', **class_attr)
+    # Header for color key
+    tr = soup.new_tag('tr')
+    td = soup.new_tag('th', scope='col')
+    td.string = "Color Key"
+    tr.append(td)
+    table.append(tr)
+    # Rows for colors
+    #ROW 1
+    tr = soup.new_tag('tr')
+    td = soup.new_tag('td', style='background:#F1948A')
+    td.string = "Setting exists in GPO 1 but not in GPO 2."
+    tr.append(td)
+    table.append(tr)
+    #ROW 2
+    tr = soup.new_tag('tr')
+    td = soup.new_tag('td', style='background:#BB8FCE')
+    td.string = "Setting exists in GPO 2 but not in GPO 1."
+    tr.append(td)
+    table.append(tr)
+    #ROW 3
+    tr = soup.new_tag('tr')
+    td = soup.new_tag('td', style='background:#F7DC6F')
+    td.string = "Setting exists in both GPOs but has a different value."
+    tr.append(td)
+    table.append(tr)
+    #ROW 4
+    tr = soup.new_tag('tr')
+    td = soup.new_tag('td', style='background:#82E0AA')
+    td.string = "Setting is the same in both GPOs."
+    tr.append(td)
+    table.append(tr)
+    
+    s.append(table)
+    
+def update_html_delete_extra(soup, root1, root2):
+    soup = soup.find('span', text='Computer Configuration (Enabled)').parent
+    pl1, pl2 = [], []
+    update_html_delete_extra_util(root1, pl1)
+    update_html_delete_extra_util(root2, pl2)
+    pl1 = set([tuple(x) for x in pl1])
+    pl2 = set([tuple(x) for x in pl2])
+    # List of paths to be deleted from html
+    path_list = list((pl1 - pl2).union(pl2 - pl1))
+    s = [x.find_all('span', class_='sectionTitle') for x in soup.next_siblings if x != '\n']
+    s_list = [x for y in s for x in y]
+    to_del = [x[0].parent.parent for x in path_list if x[0] in [y.text for y in s_list]]
+    for d in to_del:
+        temp = d.find_next_sibling('div', class_='container')
+        if temp:
+            temp.decompose()
+        d.decompose()
+        
+def update_html_delete_extra_util(root, path_list):
+    if root.path:
+        path_list.append(root.path)
+    for c in root.children:
+        update_html_delete_extra_util(c, path_list)
+    
 # Print comparison results for each leaf node.
 def print_comparison(comparisons, outfile, quiet=False):
     with open(outfile, 'a') as file:
@@ -174,84 +304,39 @@ def print_comparison(comparisons, outfile, quiet=False):
 # Modified from:
 # stackoverflow.com/questions/1649027/how-do-i-print-out-a-tree-structure
 def print_tree(root, indent, last, outfile, quiet=False):
-    if not quiet: #quiet switch specified
-        print("%s +- %s" % (indent, root.name))
+    out_str = ("%s +- %s" % (indent, root.name))
+    if not quiet:
+        print(out_str)
     with open(outfile, 'a') as file:
-        file.write("%s +- %s\n" % (indent, root.name)) #out to file
+        file.write(out_str)
     if root.is_leaf:
         for i in root.comparisons[1:]:
+            # Longest str len in output used for padding
             max_length = max([[len(str(x)) for x in y] for y in i])[0]
             for j in i:
                 s = j[0]
                 j[0] = j[0].ljust(max_length + 1, '-')
-                if not quiet: #quiet switch specified
-                    print("%s    -> %s" % 
-                          (indent, '| '.join(str(x) for x in j)))
+                out_str = "%s    -> %s" % (indent,'| '.join(str(x) for x in j))
+                if not quiet:
+                    print(out_str)
                 with open(outfile, 'a') as file:
-                    file.write("%s    -> %s\n" % (indent, '| '.join(str(x)for x in j)))
+                    file.write(out_str)
                 j[0] = s
     indent += "   " if last else "|  "
     for i in range(len(root.children)):
         print_tree(root.children[i], indent, i == len(root.children)-1, 
                    outfile, quiet)
 
-def update_html(tables, cell, comparison):
-    for table in tables:
-        for r in table.find_all('tr'):
-            d = [x.string for x in r.find_all('td')]
-            if cell in d:
-                if str(comparison) == '1':
-                    r['style'] = 'background:#EBDEF0'
-                elif str(comparison) == '2':
-                    r['style'] = 'background:#FADBD8'
-                elif str(comparison) == '!':
-                    r['style'] = 'background:#D5F5E3'
-                return
-
-# Generate an html page to display results in a readable format.
-def generate_html(root, outfile):
-    html = ET.Element('html')
-    head = ET.Element('head')
-    body = ET.Element('body')
-    link = ET.Element('link', attrib={'rel': 'stylesheet', 
-                                      'type': 'text/css',
-                                      'href': 'theme.css'})
-    html.append(head)
-    head.append(link)
-    html.append(body)
-    
-    generate_html_util(root, body, html, outfile)
-
-def generate_html_util(root, tag, html, outfile):
-    for child in root.children:
-        div = ET.Element('div', attrib={'class': 'node',
-                                        'id': child.name})
-        tag.append(div)
-        span = ET.Element('span', attrib={'class': 'sectionTitle'})
-        div.append(span)
-        span.text = child.name
-        
-        if child.table:
-            table = ET.Element('table', attrib={'class': 'info'})
-            for row in child.table:
-                tr = ET.Element('tr')
-                for col in row:
-                    td = ET.Element('td')
-                    td.text = col
-                    tr.append(td)
-                if child.comparisons:
-                    row_idx = [x[0] for x in child.comparisons[-1]].index(row[0])
-                    td = ET.Element('td')
-                    td.text = str(child.comparisons[-1][row_idx][-1])
-                    tr.append(td)
-                table.append(tr)
-            div.append(table)
-        
-        generate_html_util(child, div, html, outfile)
-    ET.ElementTree(html).write(outfile, encoding='utf8', method='html')
-
-
+# =============================================================================
+# =============================================================================
 if __name__ == '__main__':
+    # Lists used to store all leaf nodes. Makes life easier in comparison.
+    leaf_list1 = []
+    leaf_list2 = []
+    
+    # =================
+    # Argument Handling
+    # =================
     parser = argparse.ArgumentParser(description='Compare two GPOs.')
     parser.add_argument('gpos', nargs=2, 
                         help='Two GPO .html file paths to compare.')
@@ -260,7 +345,11 @@ if __name__ == '__main__':
     parser.add_argument('-q', '--quiet', action='store_true',
                         help='Boolean switch to suppress command line output.')
     args = parser.parse_args()
-    # Handle input file validation
+    
+    # ================
+    # Input Validation
+    # ================
+    
     if Path(args.gpos[0]).exists():
         url1 = "file:" + args.gpos[0]
     else:
@@ -269,13 +358,21 @@ if __name__ == '__main__':
         url2 = "file:" + args.gpos[1]
     else:
         raise OSError('GPO file {} does not exist.'.format(args.gpos[1]))
-    # Handle output file validation
+    
+    # =================
+    # Output Validation
+    # =================
+    
     outfile = args.output
     try:
         with open(outfile, 'w') as file:
             file.write('')
     except IOError:
         print('Output file error for {}.'.format(outfile))
+    
+    # ==================
+    # BS4 Initialization
+    # ==================
     
     response1 = urlopen(url1).read()
     soup1 = BeautifulSoup(response1, 'lxml')
@@ -285,19 +382,38 @@ if __name__ == '__main__':
     soup2 = BeautifulSoup(response2, 'lxml')
     body2 = soup2.find('body')
     
-    # Lists used to store all leaf nodes. Makes life easier in comparison.
-    leaf_list1 = []
-    leaf_list2 = []
+    
+    
+    # ===============
+    # Tree Generation
+    # ===============
     
     root = Tree()
     root.name = url1 + ' v ' + url2
     tree1, leaf_list1 = build_tree(body1, root, leaf_list1)
+    
     root = Tree()
     root.name = url2 + ' v ' + url1
     tree2, leaf_list2 = build_tree(body2, root, leaf_list2)
     
+    # Compare the two trees and update their node contents.
     comparison = compare_trees(leaf_list1, leaf_list2)
-    # Overwrite file
+    
+    # ===============
+    # HTML Generation
+    # ===============
+    
+    update_html_general_section(soup1, args.gpos[0], args.gpos[1])
+    update_html_delete_extra_util(soup1, tree1, tree2)
+    
+    html_out = soup1.prettify('utf-8')
+    with open('updated_{}'.format(args.gpos[0]), 'wb') as file:
+        file.write(html_out)
+        
+    # ================
+    # Text File Output
+    # ================
+    
     with open(outfile, 'w') as file:
         file.write('KEY\n'
                    'GPO1: {}\n'.format(url1) +
@@ -307,14 +423,6 @@ if __name__ == '__main__':
                    '=: Setting is the same in both GPOs\n'
                    '!: Setting exists in both GPOs but has different values.\n'
                    '________________________________________________________\n')
+        
     print_tree(tree1, '', True, outfile, args.quiet)
-    
     print_comparison(comparison, outfile, args.quiet)
-    
-    html_out = soup1.prettify('utf-8')
-    with open('updated_{}'.format(args.gpos[0]), 'wb') as file:
-        file.write(html_out)
-    
-    html_out = soup2.prettify('utf-8')
-    with open('updated_{}'.format(args.gpos[1]), 'wb') as file:
-        file.write(html_out)
